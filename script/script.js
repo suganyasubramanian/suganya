e3f59f37eb99989179ff9034cb3eaa15a5325b42
suganyasubramanian <s.suganya14@tcs.com>
2014-10-21 15:35:59 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,54 @@
-$
+<script type="text/javascript">
+$(document).ready(function(){
+  $('#registrationForm').bootstrapValidator({
+      
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+  fields: {
+            username: {
+                message: 'The username is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required and cannot be empty'
+                    },
+                    stringLength: {
+                       
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                    regexp: {
+                        
+                        regexp: /^[0-9]+$/,
+                        message: 'The username can only consist of alphabetical and number'
+                    },
+                    different: {
+                        field: 'password',
+                        message: 'The username and password cannot be the same as each other'
+                    }
+                }
+            },password: {
+                validators: {
+                    notEmpty: {
+                        message: 'The password is required and cannot be empty'
+                    },
+                    different: {
+                        field: 'username',
+                        message: 'The password cannot be the same as username'
+                    },
+                    stringLength: {
+                        min: 8,
+                        message: 'The password must have at least 8 characters'
+                    }
+                }
+            }
+          }
+  });
+
+});
+
+    
+</script>
